e7acb198d151dd980b831fc77581dd31be66e05d
Amirreza <amirreza.m13825@gmail.com>
2023-10-26 20:27:20 +0330

    create the first component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <learning-resources
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :resource="resource"
+    ></learning-resources>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LearningResources from "./components/learning-resources/LearningResources.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    LearningResources,
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation",
+          link: "https://vuejs.org/",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google",
+          link: "https://google.com/",
+        },
+      ],
+    };
+  },
+};
+</script>
