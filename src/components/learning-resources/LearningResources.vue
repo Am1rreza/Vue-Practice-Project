3196a9f7cab60943b0b85d2add4dbd99255d9eb5
Amirreza <amirreza.m13825@gmail.com>
2023-10-26 21:44:02 +0330

    create the base card component

--- src/components/learning-resources/LearningResources.vue
+++ src/components/learning-resources/LearningResources.vue
@@ -1,15 +1,15 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ resource.title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-    <p>{{ resource.description }}</p>
-    <nav>
-      <a :href="resource.link" target="_blank">View Resource</a>
-    </nav>
+      <p>{{ resource.description }}</p>
+      <nav>
+        <a :href="resource.link" target="_blank">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
