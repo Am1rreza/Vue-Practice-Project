00fb2adc4866c7ad49c9d7394f67421a02f8927f
Amirreza <amirreza.m13825@gmail.com>
2023-10-27 21:23:12 +0330

    create and utlize the custom error box for our form

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,20 @@
 <template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close-dialog="confirmError"
+  >
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid!</p>
+      <p>
+        Please check all inputs and make sure you enter at least a few
+        charachters into each input field.
+      </p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">OK</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -29,13 +45,32 @@
 export default {
   inject: ["addNewResource"],
 
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDescription = this.$refs.descriptionInput.value;
       const enteredLink = this.$refs.linkInput.value;
 
+      if (
+        enteredTitle.trim() === "" ||
+        enteredDescription.trim() === "" ||
+        enteredLink.trim() === ""
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addNewResource(enteredTitle, enteredDescription, enteredLink);
+    },
+
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
